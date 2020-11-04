50b8eb45bc340b7123e2b72a3d5cb8bf1b9f8797
Zarkmend ZAN <36896649+ZamyArkreNendmed@users.noreply.github.com>
2020-11-04 12:01:56 -0400

    Update properties_class.js

--- entry/javascript/properties_class.js
+++ entry/javascript/properties_class.js
@@ -14,24 +14,24 @@ const properties_class = class {
         for (let i = 0; i < array.length; i++) {
           const object = array[i].split("=");
           if (object.length % 2 == 0) {
-	        for (let w = 0; w < object.length; w += 2) {
-	          try {
-	            result[object[w]] = object[w + 1];
-		      }
-		      catch (error) {
-		        throw Error("Error parsing to json.");
-		      };
-	        };
-	      }
-	      else {
-	      };
+            for (let w = 0; w < object.length; w += 2) {
+              try {
+                result[object[w]] = object[w + 1];
+              }
+              catch (error) {
+                throw Error("Error parsing to json.");
+              };
+            };
+          }
+          else {
+          };
         };
         return options.is_url ? URL.createObjectURL(new Blob([JSON.stringify(result, null, 2)], {"type": "application/json"})) : result;
       },
       "create_from_json": function (json_object, options = {}) {
         let result = "";
         for (const [key, value] of Object.entries(json_object)) {
-	      result += `${key}=${typeof value === "object" ? JSON.stringify(value) : value}\n`;
+          result += `${key}=${typeof value === "object" ? JSON.stringify(value) : value}\n`;
         };
         return options.is_url ? URL.createObjectURL(new Blob([result])) : result;
       }
